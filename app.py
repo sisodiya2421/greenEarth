3e122b8856d2c094253ce7537d773be516566459
Abhishek Sisodiya <sisodiya2421@gmail.com>
2021-02-12 22:13:01 +0530

    feat: add polar ice caps info

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
