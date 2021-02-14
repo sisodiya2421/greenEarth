04ac833a8e6af6a4057b64e86f11c90f8daf8f84
Abhishek Sisodiya <sisodiya2421@gmail.com>
2021-02-15 02:42:53 +0530

    feat: visualization of earth ice

--- static/script.js
+++ static/script.js
@@ -3,16 +3,17 @@ const image = document.getElementById("play_image")
 
 let srno = 2049;
 let year = 1980;
+let interval = null
 btn.addEventListener("click", () => {
-    const interval = setInterval(changeImg(), 500);
+    interval = setInterval(changeImg(), 500);
 });
 
 const changeImg = () => {
     if (year < 2021) {
       image.src = `../static/time_series/${srno}_seaice_${year}_720x360.jpg`;
-      srno++;
-      year++;
-    } 
+      srno = srno + 1;
+      year = year + 1;
+    }
     else {
       clearInterval(interval);
     }
