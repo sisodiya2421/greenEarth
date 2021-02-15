56a46a8dd6c26f761f9bbf67463a118fb69490ed
Abhishek Sisodiya <sisodiya2421@gmail.com>
2021-02-15 19:47:14 +0530

    feat: add pause feature in earth visualization

--- static/script.js
+++ static/script.js
@@ -1,11 +1,15 @@
 const image = document.getElementById("play_image")
 var slider = document.getElementById("myRange");
-const yearPlaceholder = document.getElementById("year")
+const yearPlaceholder = document.getElementById("year");
+var id;
+const play_wrapper = document.getElementById("play_wrapper");
+
+let srno = 2048;
+let year = 1979;
 
 function play() {
-  let srno = 2048;
-  let year = 1979; 
-  var id = setInterval(changeImg, 500);
+  play_wrapper.innerHTML = `<button type="button" class="btn mt-2" onclick="pauseTimer()"><img src = "../static/pause.png" alt="play" id="play" width="28px" height="28px"/></button>`; 
+  id = setInterval(changeImg, 500);
   function changeImg() {
     if (year < 2021){
       image.src = `../static/time_series/${srno}_seaice_${year}_720x360.jpg`;
@@ -15,6 +19,7 @@ function play() {
       year = year + 1;
     }
     else {
+      play_wrapper.innerHTML = `<button type="button" class="btn mt-2" onclick="play()"><img src = "../static/play.png" alt="play" id="play" width="28px" height="28px"/></button>`;
       clearInterval(id);
     }
   }
@@ -23,4 +28,9 @@ function play() {
 slider.oninput = function changeslider() {
   yearPlaceholder.innerHTML = this.value;
   image.src = `../static/time_series/${parseInt(this.value) + 69}_seaice_${this.value}_720x360.jpg`;
+}
+
+function pauseTimer(){
+  play_wrapper.innerHTML = `<button type="button" class="btn mt-2" onclick="play()"><img src = "../static/play.png" alt="play" id="play" width="28px" height="28px"/></button>`;
+  clearInterval(id)
 }
