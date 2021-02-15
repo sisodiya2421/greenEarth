a48afaf7c816c7ed842a851b93c5432577834de2
Abhishek Sisodiya <sisodiya2421@gmail.com>
2021-02-15 14:18:45 +0530

    fix: earth image change on play is fixed

--- static/script.js
+++ static/script.js
@@ -1,20 +1,17 @@
-const btn = document.querySelector("button");
 const image = document.getElementById("play_image")
 
-let srno = 2049;
-let year = 1980;
-let interval = null
-btn.addEventListener("click", () => {
-    interval = setInterval(changeImg(), 500);
-});
-
-const changeImg = () => {
-    if (year < 2021) {
+function play() {
+  let srno = 2048;
+  let year = 1979; 
+  var id = setInterval(changeImg, 500);
+  function changeImg() {
+    if (year < 2021){
       image.src = `../static/time_series/${srno}_seaice_${year}_720x360.jpg`;
       srno = srno + 1;
       year = year + 1;
     }
     else {
-      clearInterval(interval);
+      clearInterval(id);
     }
+  }
 };
